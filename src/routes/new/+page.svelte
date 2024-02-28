464cbb93bbbf96ffe6d91cd4d47fad75cb6ce386
Vaibhav Sharma <yavsvaibhav@gmail.com>
2024-02-29 01:44:37 +0530

    fixed mobile detection

--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -21,6 +21,11 @@
     // let onSurface = "#ede0dd";
 
     onMount(() => {
+        let isMobile = function() {
+            let check = false;
+            (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+            return check;
+        };
         let p5 = window.p5;
         let THREE = window.THREE;
         topologyHomeEffect = VANTA.TOPOLOGY({
@@ -37,16 +42,14 @@
             THREE,
             p5
         });
-        console.log(ScrollTrigger.isTouch === 1);
-        if (ScrollTrigger.isTouch === 1) {
-            console.log("HELLO WORLD");
+        if (isMobile()) {
             console.log("TOUCH DEVICE FOUND");
             let destroyTopologyTimeline = gsap.timeline(
                 {
                     scrollTrigger: {
                         trigger: ".featured-events-trigger",
-                        start: "top top",
-                        end: "top -10%",
+                        start: "top bottom",
+                        end: "top 65%",
                         scrub: true,
                         markers: false,
                     },
@@ -59,20 +62,20 @@
                 },
                 onReverseComplete: () => {
                     console.debug("Topology Restored");
-                        topologyHomeEffect = VANTA.TOPOLOGY({
-                            el: ".topology-animation-home",
-                            mouseControls: false,
-                            touchControls: false,
-                            gyroControls: false,
-                            minHeight: 200.0,
-                            minWidth: 200.0,
-                            scale: 1.0,
-                            scaleMobile: 100.0,
-                            color: primaryContainer,
-                            backgroundColor: "transparent",
-                            THREE,
-                            p5,
-                        });
+                    topologyHomeEffect = VANTA.TOPOLOGY({
+                        el: ".topology-animation-home",
+                        mouseControls: false,
+                        touchControls: false,
+                        gyroControls: false,
+                        minHeight: 200.0,
+                        minWidth: 200.0,
+                        scale: 1.0,
+                        scaleMobile: 100.0,
+                        color: primaryContainer,
+                        backgroundColor: "transparent",
+                        THREE,
+                        p5,
+                    });
                 }
             })
         }
@@ -96,7 +99,11 @@
             duration: 10,
         })
 
-        let onLoadTimeline = gsap.timeline();
+        let onLoadTimeline = gsap.timeline({
+            // onComplete: () => {
+            //     changeThemeTimeline.play(0);
+            // }
+        });
         onLoadTimeline.to('.load-background', {
             backgroundColor: onSurface,
             delay: 1,
@@ -154,8 +161,51 @@
         }, "<");
         onLoadTimeline.to('.load-background', {
             display: "none",
-        })
+        });
 
+        // let changeThemeTimeline = gsap.timeline({});
+        // changeThemeTimeline.pause(0);
+        // changeThemeTimeline.to(".first-division-container", {
+        //     opacity: 0,
+        //     duration: 0.05
+        // });
+        // changeThemeTimeline.to(".first-division-container", {
+        //     opacity: 1,
+        //     duration: 0.05
+        // });
+        // changeThemeTimeline.to(".first-division-container", {
+        //     opacity: 0,
+        //     duration: 0.05
+        // });
+        // changeThemeTimeline.to(".first-division-container", {
+        //     opacity: 1,
+        //     duration: 0.05
+        // });
+        // changeThemeTimeline.to(".first-division-container", {
+        //     opacity: 0,
+        //     duration: 0.05
+        // });
+        // changeThemeTimeline.to(".first-division-container", {
+        //     opacity: 1,
+        //     duration: 0.35,
+        //     // onStart: () => {
+        //     //     console.debug(document.getElementsByClassName("first-divison-tech-container"))
+        //     //     document.getElementsByClassName("first-divison-tech-container")[0].classList.add("cyber-font")
+        //     //     document.getElementsByClassName("first-divison-tech-container")[0].classList.remove("brand-font")
+        //     // }
+        // });
+        // changeThemeTimeline.to(".first-division-tech-container", {
+        //     fontFamily: "Cyber",
+        //     duration: 0,
+        // }, "<");
+        // changeThemeTimeline.to(".first-division-solstice-container", {
+        //     fontFamily: "Cyber",
+        //     duration: 0,
+        // }, "<");
+        // changeThemeTimeline.to(".first-division-solstice-container", {
+        //     fontFamily: "Cyber",
+        //     duration: 0,
+        // }, "<");
 
         let scrollAnimation = gsap.timeline({
             scrollTrigger: {
@@ -597,7 +647,6 @@
     });
 
 </script>
-
 <div class="h-screen w-full bg-primary flex flex-col items-center justify-center load-background">
     <p class="brand-font font-bold text-[140px] sm:text-[160px] md:text-[195px] lg:text-[210px] xl:text-[233px] scale-[1.4] hidden text-primary-container tracking-wide main-text leading-[0.78]">
         Welcome</p>
@@ -614,7 +663,7 @@
             <div class="col-start-2 bg-surface h-0 w-full bar-reveal self-end"></div>
             <div class="col-start-3 bg-surface h-0 w-full bar-reveal"></div>
             <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                <div class="h-fit w-full brand-font text-[140px] sm:text-[160px] md:text-[195px] lg:text-[210px] xl:text-[233px] tracking-wide hidden text-primary-container minus-blur overflow-y-clip flex-row items-center justify-start">
+                <div class="h-fit w-full brand-font text-[140px] sm:text-[160px] md:text-[195px] lg:text-[210px] xl:text-[233px] tracking-wide hidden text-primary-container minus-blur overflow-y-clip flex-row items-center justify-start first-division-tech-container">
                     <span class="translate-y-14 opacity-0 letter-up-1 left-of-make-italic leading-[0.88]">T</span><span
                         class="translate-y-[70px] opacity-0 letter-up-2 make-italic leading-[0.88]">E</span><span
                         class="translate-y-[90px] opacity-0 letter-up-3 leading-[0.88]">C</span><span
@@ -625,7 +674,7 @@
                         </p>
                     </div>
                 </div>
-                <div class="h-fit w-full brand-font text-[155px] sm:text-[175px] md:text-[210px] lg:text-[210px] xl:text-[275px] tracking-wide hidden text-primary minus-blur overflow-y-clip flex-row items-center justify-center">
+                <div class="h-fit w-full brand-font text-[155px] sm:text-[175px] md:text-[210px] lg:text-[210px] xl:text-[275px] tracking-wide hidden text-primary minus-blur overflow-y-clip flex-row items-center justify-center first-division-solstice-container">
                     <span class="translate-y-14 opacity-0 letter-up-1 leading-[0.88]">S</span>
                     <span
                             class="translate-y-[70px] opacity-0 letter-up-2 left-of-make-italic leading-[0.88]">O</span><span
