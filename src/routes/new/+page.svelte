bda001f5f96e030482ac5124d724716197c33f2e
Vaibhav Sharma <yavsvaibhav@gmail.com>
2024-02-28 12:00:23 +0530

    changed mobile scale

--- src/routes/new/+page.svelte
+++ src/routes/new/+page.svelte
@@ -29,7 +29,7 @@
             minHeight: 200.00,
             minWidth: 200.00,
             scale: 1.00,
-            scaleMobile: 2.00,
+            scaleMobile: 9.00,
             color: primaryContainer,
             backgroundColor: "transparent",
             p5
