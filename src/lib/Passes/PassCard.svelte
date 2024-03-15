52a3d007d3302322eabae366a6a88cb89f74ac8f
Vaibhav Sharma <yavsvaibhav@gmail.com>
2024-03-16 03:56:22 +0530

    changed buy to coming soon

--- src/lib/Passes/PassCard.svelte
+++ src/lib/Passes/PassCard.svelte
@@ -107,40 +107,40 @@
                     {/each}
                 </div>
             </div>
-            <button
-                    class="{buttonBgColorClass} text-2xl flex flex-row items-center justify-center gap-3 {buttonTextColorClass} brand-font px-5 py-1 flagship-buy-button group hover:bg-on-surface hover:text-surface"
-                    on:click={async () => {
-                                    $clickedPassRedirectToken = redirectToken;
-                                 if(!$page.data.session?.user) {
-                                   animateLoadingPhase();
-                                     await signIn('google', {callbackUrl: `${$page.url.pathname}?signedIn`})
-                                 } else {
-                                     if(!userData) {
-                                         showDataForm();
-                                     } else {
-                                        animateLoadingPhase();
-                                        await goto(`/payment/${redirectToken}`)
-                                     }
-                                 }
-                            }}
-            >
-                {#if $page.data.session?.user}
-                    <p class="button-inner-text">BUY</p>
-                {:else}
-                    <p class="button-inner-text">LOGIN</p>
-                {/if}
-                <div class="h-full w-full flex-col items-center justify-center loader-buy hidden scale-0">
-                    <div class="rounded-full bg-on-primary h-8 w-8 loader"></div>
-                </div>
-                <div class="{buttonPriceBgColorClass} {buttonPriceTextColorClass} group-hover:bg-primary group-hover:text-on-primary brand-font text-2xl flex flex-row items-center justify-center px-4 py-1 flagship-buy-text">
-                    <p>₹{price}</p>
-                </div>
-            </button>
-            <!--            <button class="{buttonBgColorClass} text-2xl flex flex-row items-center justify-center-->
-            <!--                gap-3 {buttonTextColorClass} brand-font px-5 py-1 flagship-buy-button group hover:bg-on-surface-->
-            <!--                hover:text-surface">-->
-            <!--                Coming Soon-->
-            <!--            </button>-->
+<!--            <button-->
+<!--                    class="{buttonBgColorClass} text-2xl flex flex-row items-center justify-center gap-3 {buttonTextColorClass} brand-font px-5 py-1 flagship-buy-button group hover:bg-on-surface hover:text-surface"-->
+<!--                    on:click={async () => {-->
+<!--                                    $clickedPassRedirectToken = redirectToken;-->
+<!--                                 if(!$page.data.session?.user) {-->
+<!--                                   animateLoadingPhase();-->
+<!--                                     await signIn('google', {callbackUrl: `${$page.url.pathname}?signedIn`})-->
+<!--                                 } else {-->
+<!--                                     if(!userData) {-->
+<!--                                         showDataForm();-->
+<!--                                     } else {-->
+<!--                                        animateLoadingPhase();-->
+<!--                                        await goto(`/payment/${redirectToken}`)-->
+<!--                                     }-->
+<!--                                 }-->
+<!--                            }}-->
+<!--            >-->
+<!--                {#if $page.data.session?.user}-->
+<!--                    <p class="button-inner-text">BUY</p>-->
+<!--                {:else}-->
+<!--                    <p class="button-inner-text">LOGIN</p>-->
+<!--                {/if}-->
+<!--                <div class="h-full w-full flex-col items-center justify-center loader-buy hidden scale-0">-->
+<!--                    <div class="rounded-full bg-on-primary h-8 w-8 loader"></div>-->
+<!--                </div>-->
+<!--                <div class="{buttonPriceBgColorClass} {buttonPriceTextColorClass} group-hover:bg-primary group-hover:text-on-primary brand-font text-2xl flex flex-row items-center justify-center px-4 py-1 flagship-buy-text">-->
+<!--                    <p>₹{price}</p>-->
+<!--                </div>-->
+<!--            </button>-->
+                        <button class="{buttonBgColorClass} text-2xl flex flex-row items-center justify-center
+                            gap-3 {buttonTextColorClass} brand-font px-5 py-1 flagship-buy-button group hover:bg-on-surface
+                            hover:text-surface">
+                            Coming Soon
+                        </button>
         </div>
     {:else}
         <div class="h-full w-full flex flex-col">
